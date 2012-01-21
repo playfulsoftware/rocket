127b0fdded1a6416e633636be5c0fc3d89a271a5
Cary Haynie <cary.haynie@gmail.com>
2012-01-21 16:20:35 -0500

    add lua vm class.

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,46 @@
 #include <private/SimpleEventListener.hpp>
 #include <private/SimpleEventSource.hpp>
 
+class LuaVM {
+
+    private:
+        lua_State* vmState;
+
+    public:
+        LuaVM() {
+            vmState = luaL_newstate();
+            luaL_openlibs(vmState);
+        }
+
+        ~LuaVM() {
+            lua_close(vmState);
+        }
+
+        void runScriptFile(const char* file) {
+            if (luaL_dofile(vmState, file) != 0) {
+                std::cerr << "an error occurred while executing the script file: " << file << std::endl;
+            }
+        }
+
+        void runScriptString(const char* buffer) {
+            if (luaL_dostring(vmState, buffer) != 0) {
+                std::cerr << "an error occurred while executing the script buffer" << std::endl;
+            }
+
+        }
+};
+
 int main(int argc, char** argv) {
     SimpleEvent e("MyEvent");
     SimpleEventListener l;
     SimpleEventSource s;
 
+    LuaVM vm;
+
     s.addListener(&l);
     s.emit(&e);
 
-    std::cout << "creating lua vm" << std::endl;
-    lua_State* state = luaL_newstate();
-    std::cout << "opening std libraries" << std::endl;
-    luaL_openlibs(state);
-    std::cout << "running lua test script." << std::endl;
-    luaL_dostring(state, "print(\"Hello, PSEngine!\")");
-    std::cout << "closing lua vm" << std::endl;
-    lua_close(state);
+    vm.runScriptString("print(\"Hello!\")");
 
     return 0;
 }

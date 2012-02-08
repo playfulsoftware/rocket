041634726a898279ecf142a3ba354d76239365e9
Cary Haynie <cary.haynie@gmail.com>
2012-02-07 22:29:42 -0500

    updated lua script.

--- src/test.lua
+++ src/test.lua
@@ -2,6 +2,10 @@ on_event("MyEvent", function (evt)
     trace("I saw event: " .. evt)
 end)
 
+on_event("MyEvent", function(evt)
+    trace("I also saw event: " .. evt)
+end)
+
 trace()
 
 trace("A test!")

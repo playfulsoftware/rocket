3d4253e91596c56b31f42ccb1e0d2c25bce12487
Cary Haynie <cary.haynie@gmail.com>
2012-05-03 23:57:23 -0400

    adding new event system based on fast delegate

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,56 @@
+#include <deque>
 #include <iostream>
-#include <vector>
+#include <string>
 
 #include <SDL/SDL.h>
 
 #include "FastDelegate.h"
 #include "LuaVM.h"
 
-template<class Handler>
-class Event {
+using namespace fastdelegate;
+
+class EventArgs { 
     public:
-        typedef Handler HandlerType;
-
-        void on(HandlerType handler) {
-            HandlerTypeInfo hti;
-            hti.handler = handler;
-            hti.once = false;
-            this.handlers.push_back(hti);
-        }
-        void once(HandlerType handler) {
-            HandlerTypeInfo hti;
-            hti.handler = handler;
-            hti.once = true;
-            this.handlers.push_back(hti);
-        }
-    private:
-        typedef struct {
-            HandlerType handler;
-            bool once;
-        } HandlerTypeInfo;
-
-        std::vector<HandlerTypeInfo> handlers;
+        virtual const char* getEventName() = 0;
+        virtual void* getEventData() = 0;
 };
 
+typedef FastDelegate<void (const EventArgs &)> EventHandler;
 
+// We differentiate between arguments to allow for the binding of subclasses
+// to an ancestor's member function pointer.
+template <class X, class Y>
+EventHandler makeHandler(X* klass, void (Y::*func_ptr)(const EventArgs&)) {
+    return EventHandler(klass, func_ptr);
+}
 
+class Event {
+    public:
+        void on(const EventHandler& handler) {
+            handlers.push_back(handler);
+        }
 
+        void operator()(const EventArgs &e) {
+            std::deque<EventHandler>::iterator iter = handlers.begin();
+            for (iter; iter != handlers.end(); ++iter) {
+                (*iter)(e);
+            }
+        }
+
+    private:
+        std::deque<EventHandler> handlers;
+};
+
+class SDLEventArgs : public EventArgs {
+    public:
+        SDLEventArgs(std::string evt_name) { name = evt_name; }
+
+        virtual const char* getEventName() { return name.c_str(); }
+        virtual void* getEventData() { return NULL; }
+
+    private:
+        std::string name;
+};
 
 class SDLEngineLoop{
 
@@ -42,6 +58,9 @@ class SDLEngineLoop{
         bool shouldRun;
 
     public:
+
+        Event engineEvents;
+
         SDLEngineLoop() : shouldRun(true) {
             if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
                 std::cerr << "fatal error: unable to initialize SDL" << std::endl;
@@ -57,7 +76,10 @@ class SDLEngineLoop{
         }
 
         int start() {
-
+            std::cerr << "sending start loop message" << std::endl;
+            SDLEventArgs start("SDLLoopStart");
+            engineEvents(start);
+            std::cerr << "message sent" << std::endl;
 
             SDL_Event evt;
 
@@ -79,9 +101,25 @@ class SDLEngineLoop{
 
 };
 
+void test(const EventArgs& e) {
+    std::cerr << "in event handler func" << std::endl;
+}
+
+class TestClass {
+    public:
+        void func(const EventArgs& e) {
+            std::cerr << "in member event handler func" << std::endl;
+        }
+};
+
 int main(int argc, char** argv) {
 
     SDLEngineLoop main;
+
+    TestClass t;
+
+    main.engineEvents.on(EventHandler(test));
+    main.engineEvents.on(EventHandler(&t, &TestClass::func));
     //LuaVM vm;
 
     //if (!vm.runScriptFile("test.lua")) {
